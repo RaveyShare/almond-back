23a2746c2b6f49bd934aca663b330a4ae8418359
Ravey <15132698828@163.com>
2025-12-15 09:07:16 +0800

    Initial commit

---
+++ almond-back-start/src/main/java/com/ravey/almond/start/AlmondBackApplication.java
@@ -0,0 +1,24 @@
+package com.ravey.almond.start;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Ravey
+ * @since 1.0.0
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ravey.almond"})
+@MapperScan("com.ravey.almond.service.dao.mapper")
+public class AlmondBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmondBackApplication.class, args);
+    }
+}
